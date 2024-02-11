2527499a0ecbcafe8c45a7dbbd8705c7bc1ac058
tkassila <tuomas.kassila@gmail.com>
2024-02-11 16:53:05 +0200

    run.sh +current jar

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ export rundir
 #echo rundir=$rundir
 # icu is not needed any more:
 # $rundir/lib/icu4j-70.1.jar:
-java -cp $rundir/javafxwebpages.jar:$rundir/lib/gson-2.10.1.jar:$CLASSPATH com.metait.javafxwebpages.WebPagesApplication
+java -cp $rundir/javafxwebpages-1.0.jar:$rundir/lib/gson-2.10.1.jar:$CLASSPATH com.metait.javafxwebpages.WebPagesApplication
